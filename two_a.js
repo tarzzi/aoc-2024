0930a42ece15492f603e7ac4e64ff2adacc3e6f3
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2024-12-03 20:50:59 +0200

    Ternary fun

--- two_a.js
+++ two_a.js
@@ -6,13 +6,11 @@ for (let i = 0; i < inputArray.length; i++) {
   let safe = true;
   let dirarray = [];
   for (let k = 0; k < row.length - 1; k++) {
-    dirarray.push((row[k] - row[k + 1]) == 0 ? 0 : (row[k] - row[k + 1]) < 0 ? -1 : 1);
-    let diff = Math.abs(row[k] - row[k + 1]);
-    if (diff < 1 || diff > 3) {
-      safe = false;
-    }
+    let sub = row[k] - row[k + 1];
+    dirarray.push(sub == 0 ? 0 : sub < 0 ? -1 : 1);
+    const diff = Math.abs(row[k] - row[k + 1]);
+    safe = (diff < 1 || diff > 3) ? false : safe;
   }
   let unique = [...new Set(dirarray)];
-  safeRows = (unique.length > 1 || !safe) ? safeRows : (safeRows + 1);
+  safeRows = !(unique.length > 1 || !safe) ? (safeRows + 1) : safeRows;
 }
-console.log('Safe rows', safeRows);
